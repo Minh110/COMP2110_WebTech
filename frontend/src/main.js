31fbd806bc57b9f8059cd08e25012723dd552e59
Minh110 <68814089+Minh110@users.noreply.github.com>
2022-04-25 13:18:05 +1000

    Change the link and main.js for routing the path

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,51 @@
-window.onload = () => {
-  document.getElementById("main").innerHTML = "<p>Bob's Job</p>";
+import { Router } from "./router.js";
+
+let about = {
+  content:
+    "Bob's Jobs is a revolution in career planning brought to you by Bob Bobalooba himself!",
 };
+
+let applicantHelp = { content: "Be sure to he honest in your application!" };
+
+const infoTemplate = Handlebars.compile(`
+    <div class=info>
+        <p>Name: {{name}}</p>
+        <p>Age: {{age}}</p>
+    </div>`);
+
+const aboutTemplate = Handlebars.compile(`
+    <div class=about>
+        <p>{{content}}}</p>
+    </div>
+    `);
+
+function infoView(targetid, info) {
+  const list = aboutTemplate(info);
+  const target = document.getElementById(targetid);
+
+  target.innerHTML = list;
+}
+
+const errorView = () => {
+  const target = document.getElementById("content");
+  target.innerHTML = "<p> Page not found </p>";
+};
+
+const route = new Router(errorView);
+
+route.get("/about", () => {
+  infoView("content", about);
+});
+
+route.get("/help", () => {
+  infoView("content", applicantHelp);
+});
+
+function redraw() {
+  route.route();
+}
+
+//window.onload = redraw;
+window.onhashchange = redraw;
+
+window.onload = redraw;
